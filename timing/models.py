996ecdf4ac8d295806ae0b4aaa14b6242de269ec
Nate Vack <njvack@wisc.edu>
2011-02-09 22:37:55 +0000

    Lots of model redesign
    Add StampedTrackedModel abstract base class (all others inherit from this, get automatic timestamp and code revision recording
    
    Merge Login into Participant
    
    Break Keypresses into a whole set of individual Response instances -- instead of serializing a giant array, we'll store the individual responses.

--- timing/models.py
+++ timing/models.py
@@ -1,16 +1,36 @@
 from django.db import models
+from django.conf import settings
 
-class Login(models.Model):
-    email = models.CharField(max_length=20)
-    pptID = models.IntegerField()
+"""
+Abstract base class -- automatically records created_at, updated_at,
+and SCM revision of the code used to create the record.
 
-class Participant(models.Model):
-    pptID = models.IntegerField()
+All models should inherit from this unless there's a good reason.
+"""
 
-class Run(models.Model):
-    ppt = models.ForeignKey(Participant)
-    date = models.DateTimeField()
+class StampedTrackedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
-class Keypresses(models.Model):
+    scm_revision = models.CharField(
+        "Code revision", 
+        max_length=255, 
+        default=settings.REPOSITORY_URL,
+        editable=False)
+    
+    class Meta:
+        abstract = True
+
+class Participant(StampedTrackedModel):
+    email = models.CharField(max_length=255, unique=True)
+    participant_number = models.CharField(max_length=8, unique=True)
+
+
+class Run(StampedTrackedModel):
+    participant = models.ForeignKey(Participant)
+    finished_at = models.DateTimeField(blank=True, null=True)
+    
+class Response(StampedTrackedModel):
     run = models.ForeignKey(Run)
-    keypresses = models.TextField()
+    char = models.CharField(max_length=1)
+    ms_since_run_start = models.IntegerField()

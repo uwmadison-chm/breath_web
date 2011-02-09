f9f59cd3f071531da7693a6cb43a0d88e3706053
Nate Vack <njvack@wisc.edu>
2011-02-09 21:05:51 +0000

    Strip old styles, install compass for css management

---
+++ compass/config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../public/css"
+sass_dir = "scss"
+images_dir = "../public/img"
+javascripts_dir = "../public/js"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = true

28ebaee420eece9df759280603f8cf980e484a22
Nate Vack <njvack@wisc.edu>
2011-02-17 16:29:05 +0000

    Add manage.py options to export demographics data and dictionary

---
+++ timing/management/commands/export_demographics_data.py
@@ -0,0 +1,34 @@
+import sys
+import csv
+
+from django.core.management.base import NoArgsCommand
+
+from timing import models
+
+class Command(NoArgsCommand):
+    help = "Print demographic data as CSV"
+    
+    def handle_noargs(self, **options):
+
+        field_names = [
+             'participant_number'
+            ,'gender_id'
+            ,'handedness_id'
+            ,'race_id'
+            ,'ethnicity_id'
+            ,'country_of_residence_id'
+            ,'education_level_id'
+            ,'spirituality_id'
+            ,'religious_affiliation_id'
+            ,'political_identity_id'
+            ,'occupation_id'
+            ,'birth_year'
+            ,'birth_month'
+        ]
+
+        writer = csv.writer(sys.stdout, delimiter=",")
+        writer.writerow(field_names)
+        for ppt in models.Participant.objects.all():
+            writer.writerow([
+                getattr(ppt, field) for field in field_names
+            ])
---
+++ timing/management/commands/export_demographics_dict.py
@@ -0,0 +1,36 @@
+import sys
+import csv
+
+from django.core.management.base import NoArgsCommand
+
+from timing import models
+
+class Command(NoArgsCommand):
+    help = "Print ID -> label mappings for demographic data"
+    
+    def handle_noargs(self, **options):
+        writer = csv.writer(sys.stdout, delimiter=",")
+        header_row = ['Field', 'id', 'label']
+        writer.writerow(header_row)
+        
+        demographic_classes = [
+             models.Handedness
+            ,models.Spirituality
+            ,models.Gender
+            ,models.Race
+            ,models.Occupation
+            ,models.Ethnicity
+            ,models.ReligiousAffiliation
+            ,models.PoliticalIdentity
+            ,models.CountryOfResidence
+            ,models.EducationLevel
+        ]
+        
+        for klass in demographic_classes:
+            results = klass.objects.all()
+            for row in results:
+                writer.writerow([
+                    klass.__name__,
+                    row.pk,
+                    row.label
+                ])

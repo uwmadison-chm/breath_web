8952f5fb440cbb5f83b98f5fb16c76fc8e79e0d9
Nate Vack <njvack@wisc.edu>
2011-04-11 17:59:03 +0000

    Add email outputter.

---
+++ timing/management/commands/export_emails.py
@@ -0,0 +1,20 @@
+import sys
+import csv
+
+from django.core.management.base import NoArgsCommand
+
+from timing import models
+
+class Command(NoArgsCommand):
+    help = "Print demographic data as CSV"
+    
+    def handle_noargs(self, **options):
+        ppts = models.Participant.objects.all().order_by('pk')
+        
+        field_names = ['participant_number', 'email']
+        writer = csv.writer(sys.stdout, delimiter=",")
+        writer.writerow(field_names)
+        for ppt in ppts:
+            writer.writerow([
+                getattr(ppt, field) for field in field_names
+            ])        
